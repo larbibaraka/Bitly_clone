4aae9b04bdf368a303062e46bf74a394b17f5be7
Baraka Larbi <larbibaraka@gmail.com>
2017-02-07 22:12:23 +0100

    Initial Commit

--- bitly_clone/bitly__clone_app/models.py
+++ bitly_clone/bitly__clone_app/models.py
@@ -1,5 +1,43 @@
 from __future__ import unicode_literals
 
+import random
+import string
+
 from django.db import models
 
 # Create your models here.
+
+def generate(nb_caracteres):
+    caracteres = string.ascii_letters + string.digits
+    aleatoire = [random.choice(caracteres) for _ in range(nb_caracteres)]
+
+    code = ''.join(aleatoire)
+    return code
+
+class UrlShort(models.Model):
+    user_url=models.URLField(unique=True, verbose_name='the user url')
+    url_code=models.CharField(max_length=6,unique=True)
+    date_creation=models.DateTimeField(auto_now_add=True,auto_now=False,verbose_name='creation date')
+    update_time=models.DateTimeField(auto_now_add=True,auto_now=False,verbose_name='creation date')
+    nike_name=models.CharField(max_length=20,null=True,blank=True,verbose_name='user nike name')
+    nb_access=models.IntegerField(default=0)
+
+
+
+    def __str__(self):
+        return self.user_url
+
+
+    def __unicode__(self):
+        return unicode(self.user_url)
+
+
+
+    """this methode is used to override the save methode"""
+    def save(self,*args,**kwargs):
+        self.url_code=generate(6)
+        super(UrlShort, self).save(*args,**kwargs)
+
+
+
+
--- bitly_clone/bitly_clone/settings.py
+++ bitly_clone/bitly_clone/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bitly__clone_app',
+
 ]
 
 MIDDLEWARE = [
